2dc42748ea5a7804b05e87b9a2ffacf7d1c69167
Freddy Fredericks <brandon.fredericks@craftsy.com>
2015-06-17 23:07:54 -0600

    Set it back to photo_filename instead of photo_url.

--- players/models.py
+++ players/models.py
@@ -6,7 +6,7 @@ from utilities.fields import BoundedBigAutoField
 class Player(models.Model):
     id = BoundedBigAutoField(primary_key=True)
     name = models.CharField(blank=False, max_length=100)
-    photo_url = models.CharField(blank=False, max_length=255)
+    photo_filename = models.CharField(blank=False, max_length=255)
     star = models.BooleanField(default=False, blank=True)
 
     created = models.DateTimeField(auto_now_add=True, blank=False)
