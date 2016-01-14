33ccbdda425180d95cb13fd6a30124ab69433ff0
Freddy Fredericks <brfredericks@gmail.com>
2016-01-14 14:16:42 -0700

    Finished the forums.

--- forums/views.py
+++ forums/views.py
@@ -2,11 +2,27 @@ import datetime
 import pytz
 
 from django.views.generic import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from forums.models import Forum, Thread, Reply
 from channels import service as channels_service
 from users import service as users_service
+
+
+def get_paginated(request, objects, per_page=2):
+    paginator = Paginator(objects, per_page) # How many per page
+
+    page = request.GET.get('page')
+    try:
+        paginated = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        paginated = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        paginated = paginator.page(paginator.num_pages)
+    return paginated
 
 
 class ForumHomeView(View):
@@ -26,10 +42,14 @@ class ForumHomeView(View):
 class ForumThreadListView(View):
     template_name = 'forums/forum_thread_list.html'
 
+    def paginated_threads(self, request, forum):
+        thread_list = Thread.objects.filter(forum=forum).order_by('-sticky','-updated')
+        return get_paginated(request, thread_list)
+
     def get(self, request, *args, **kwargs):
         forum_name = kwargs.get('forum_name')
         forum = get_object_or_404(Forum, name=forum_name)
-        threads = Thread.objects.filter(forum=forum).order_by('-sticky','-updated')
+        threads = self.paginated_threads(request, forum)
         admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
         user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
         return render(request,
@@ -37,32 +57,95 @@ class ForumThreadListView(View):
                       {'forum': forum,
                        'threads': threads,
                        'user_profile': user_profile,
-                       'admin_channels': admin_channels})
+                       'admin_channels': admin_channels,
+                       'page': int(request.GET.get('page', 1))})
+
+    def post(self, request, *args, **kwargs):
+        forum_name = kwargs.get('forum_name')
+        forum = get_object_or_404(Forum, name=forum_name)
+        admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
+        user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
+        delete_thread = request.POST.get('delete_thread')
+        # Super user delete thread
+        if request.user.is_superuser and delete_thread:
+            # Delete all the replies
+            for reply in Reply.objects.filter(thread=delete_thread):
+                reply.delete()
+            # Delete the thread
+            Thread.objects.get(id=delete_thread).delete()
+        threads = self.paginated_threads(request, forum)
+
+        return render(request,
+                      self.template_name,
+                      {'forum': forum,
+                       'threads': threads,
+                       'user_profile': user_profile,
+                       'admin_channels': admin_channels,
+                       'page': int(request.GET.get('page', 1))})
 
 
 class ForumCreateThreadView(View):
-    template_name = 'forums/forum_thread_list.html'
+    template_name = 'forums/forum_create_thread.html'
 
     def get(self, request, *args, **kwargs):
-        forums = Forum.objects.all()
+        error = False
+        forum_name = kwargs.get('forum_name')
+        forum = get_object_or_404(Forum, name=forum_name)
         admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
         user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
         return render(request,
                       self.template_name,
-                      {'forums': forums,
+                      {'forum': forum,
                        'user_profile': user_profile,
                        'admin_channels': admin_channels})
+
+    def post(self, request, *args, **kwargs):
+        error = False
+        forum_name = kwargs.get('forum_name')
+        forum = get_object_or_404(Forum, name=forum_name)
+        admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
+        user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+        # Forum user posting a new thread
+        if subject and message:
+            creation_datetime = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+            Thread.objects.get_or_create(
+                  forum=forum,
+                  subject=subject,
+                  message=message,
+                  sticky=False,
+                  creator=request.user,
+                  created=creation_datetime,
+                  updated=creation_datetime)
+        else:
+            error = "Subject and message must not be empty"
+
+        if error:
+            return render(request,
+                          self.template_name,
+                          {'error': error,
+                           'forum': forum,
+                           'user_profile': user_profile,
+                           'admin_channels': admin_channels,
+                           'page': int(request.GET.get('page', 1))})
+        else:
+            return redirect('forums_thread_list', forum_name=forum.name)
 
 
 class ForumThreadView(View):
     template_name = 'forums/forum_thread.html'
+
+    def paginated_replies(self, request, thread):
+        reply_list = Reply.objects.filter(thread=thread)
+        return get_paginated(request, reply_list)
 
     def get(self, request, *args, **kwargs):
         forum_name = kwargs.get('forum_name')
         thread_id = kwargs.get('thread_id')
         forum = get_object_or_404(Forum, name=forum_name)
         thread = get_object_or_404(Thread, id=thread_id)
-        replies = Reply.objects.filter(thread=thread)
+        replies = self.paginated_replies(request, thread)
         admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
         user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
         return render(request,
@@ -71,7 +154,8 @@ class ForumThreadView(View):
                        'thread': thread,
                        'replies': replies,
                        'user_profile': user_profile,
-                       'admin_channels': admin_channels})
+                       'admin_channels': admin_channels,
+                       'page': int(request.GET.get('page', 1))})
 
     def post(self, request, *args, **kwargs):
         error = False
@@ -79,11 +163,15 @@ class ForumThreadView(View):
         thread_id = kwargs.get('thread_id')
         forum = get_object_or_404(Forum, name=forum_name)
         thread = get_object_or_404(Thread, id=thread_id)
-        replies = Reply.objects.filter(thread=thread)
         admin_channels = channels_service.get_channels_by_admin(getattr(request.user, 'id'))
         user_profile = users_service.fetch_user_profile(getattr(request.user, 'id'))
         message = request.POST.get('message', '')
-        if message:
+        delete_reply = request.POST.get('delete_reply')
+        # Superuser deleting a thread reply
+        if request.user.is_superuser and delete_reply:
+            Reply.objects.get(id=delete_reply).delete()
+        # Forum user posting a reply
+        elif message:
             Reply.objects.get_or_create(
                   forum=forum,
                   thread=thread,
@@ -92,6 +180,7 @@ class ForumThreadView(View):
                   created=datetime.datetime.utcnow().replace(tzinfo=pytz.utc))
         else:
             error = "Message must not be empty"
+        replies = self.paginated_replies(request, thread)
 
         return render(request,
                       self.template_name,
@@ -100,4 +189,5 @@ class ForumThreadView(View):
                        'thread': thread,
                        'replies': replies,
                        'user_profile': user_profile,
-                       'admin_channels': admin_channels})
+                       'admin_channels': admin_channels,
+                       'page': int(request.GET.get('page', 1))})
